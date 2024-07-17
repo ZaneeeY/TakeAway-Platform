4da054579e8be568e61d86284f166c44ae960375
Zheng YU <zyuu0045@gmail.com>
2024-07-17 17:11:04 +1000

    修改菜品代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -49,4 +49,7 @@ public interface DishMapper {
      */
     @Delete("delete from dish where id = #{id}")
     void deleteById(Long id);
+
+    @AutoFill(OperationType.INSERT)
+    void update(Dish dish);
 }
